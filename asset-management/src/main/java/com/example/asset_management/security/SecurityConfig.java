5c2d851a411c48140ba92386eb8fd8714fe04376
bagale-vaishnavi <vaishnavibagale2003@gmail.com>
2026-07-23 17:51:09 +0530

    Allow All Vercel Deployments

--- asset-management/src/main/java/com/example/asset_management/security/SecurityConfig.java
+++ asset-management/src/main/java/com/example/asset_management/security/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173" , "https://assetflow-project.vercel.app",s"https://assetflow-project-4itmiwmji-bagalevaishnavi.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173" , "https://assetflow-project.vercel.app","https://assetflow-project-4itmiwmji-bagalevaishnavi.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
